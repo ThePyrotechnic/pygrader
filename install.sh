4c4725ebd255cabbbb7ee910749a3f5a0d83b164
Linux User <michael@michael-alpine-l.dhcp.stevens-tech.edu>
2018-03-20 15:15:41 -0400

    updated installation to not dump virtenv in root directory

--- install.sh
+++ install.sh
@@ -4,12 +4,13 @@
 # TODO Translate this file into a setup.py and make
 # the grader pip-installable.
 
-git clone https://github.com/ThePyrotechnic/pygrader.git
-cd pygrader
+mkdir venv
+cd venv
 python3.6 -m venv .
 source bin/activate
+cd ..
 pip install -r requirements.txt
 chmod +x pycanvasgrader.py
 # In the following commands replace $EDITOR with your editor of choice.
-echo -n "Enter your token: " && read token
+echo -n "Enter your Canvas API token: " && read token
 echo $token > access.token
