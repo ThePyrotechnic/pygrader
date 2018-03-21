98b9cf8143008282bc1eee968e24adbd1ebe8928
Edward Minnix III <egregius313@gmail.com>
2018-03-21 11:47:19 -0400

    Removed unnecessary directory switching

--- install.sh
+++ install.sh
@@ -4,11 +4,8 @@
 # TODO Translate this file into a setup.py and make
 # the grader pip-installable.
 
-mkdir venv
-cd venv
-python3.6 -m venv .
-source bin/activate
-cd ..
+python3.6 -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
 chmod +x pycanvasgrader.py
 # In the following commands replace $EDITOR with your editor of choice.
